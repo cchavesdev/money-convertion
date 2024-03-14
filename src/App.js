0298184af625ef7b08cccbaa1138ec6e8e546d1a
carlosfabianchaves <48218393+carlosfabianchaves@users.noreply.github.com>
2024-03-13 19:55:51 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TextField } from '@mui/material';
 
-function App() {
+const App = () => {
+  const colonesDollarValue = 500;
+  const cordovasDollarValue = 36;
+  const [values, setValues] = useState({
+    cordovas: '',
+    dollars: '',
+    colones: '',
+  });
+
+  const handleInputChange = (name, value) => {
+    let newValues = { ...values, [name]: value };
+
+    if (name === 'cordovas') {
+      const dollars = parseFloat(value) / 36 || 0;
+      const colones = dollars * colonesDollarValue;
+      newValues = { ...newValues, dollars: dollars.toFixed(2), colones: colones.toFixed(2) };
+    } else if (name === 'dollars') {
+      const cordovas = parseFloat(value) * 36 || 0;
+      const colones = parseFloat(value) * colonesDollarValue || 0;
+      newValues = { ...newValues, cordovas: cordovas.toFixed(2), colones: colones.toFixed(2) };
+    } else if (name === 'colones') {
+      const dollars = parseFloat(value) / colonesDollarValue || 0;
+      const cordovas = dollars * 36;
+      newValues = { ...newValues, dollars: dollars.toFixed(2), cordovas: cordovas.toFixed(2) };
+    }
+
+    setValues(newValues);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fields-container'>
+      <TextField
+        label="Cordovas"
+        variant="outlined"
+        value={values.cordovas}
+        onChange={(e) => handleInputChange('cordovas', e.target.value)}
+        onFocus={()=>{setValues({
+          cordovas: '',
+          dollars: '',
+          colones: '',
+        })}}
+        type="number"
+      />
+      <TextField
+        label="Dollars"
+        variant="outlined"
+        value={values.dollars}
+        onChange={(e) => handleInputChange('dollars', e.target.value)}
+        onFocus={()=>{setValues({
+          cordovas: '',
+          dollars: '',
+          colones: '',
+        })}}
+        type="number"
+      />
+      <TextField
+        label="Colones"
+        variant="outlined"
+        value={values.colones}
+        onChange={(e) => handleInputChange('colones', e.target.value)}
+        onFocus={()=>{setValues({
+          cordovas: '',
+          dollars: '',
+          colones: '',
+        })}}
+        type="number"
+      />
     </div>
   );
-}
+};
 
 export default App;
